c401c49e4750484fa25d83f4fe4dbea58bbd49bc
LuisDavidRuizOrtiz <l.davidcompeta@gmail.com>
2024-04-07 11:42:25 +0200

    test sqare

---
+++ percolation_testing/sq_grid.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+
+#include"gsl_rng.h"
+
+#include "trees.h"
+
+using namespace std;
+
+ofstream file;
+
+//Number of trees to generate
+int N = 100;
+//Seed for random numbers
+int seed = 932284531; 
+
+//Pointer
+gsl_rng *tau;
+
+int main() {
+
+    file.open("grid_positions.txt");
+
+    extern gsl_rng *tau;
+    tau = gsl_rng_alloc(gsl_rng_taus);
+    gsl_rng_set(tau,seed);
+
+    Tree tree[N];
+
+    for (int i = 0; i < N; i++) {
+       for(int j = 0; j < sqrt(N); j++){            //Comprueba en que row está el arbol i
+         if((((j-1)*sqrt(N))<i)&&(i<=(j*sqrt(N))))
+           tree[i].y=1.0*j;
+        }
+        for(int k = 0; k < sqrt(N); k++){
+          if((i%sqrt(N))==k)
+            tree[i].x=1.0*k;    
+        }
+
+        cout << "Tree number " << i+1 << " Pos X = " << tree[i].x << " Pos Y = " << tree[i].y << endl;
+        
+        file << tree[i].x << "," << tree[i].y << "\n";
+
+    }
+
+    file.close();
+
+    return 0;
+
+}
